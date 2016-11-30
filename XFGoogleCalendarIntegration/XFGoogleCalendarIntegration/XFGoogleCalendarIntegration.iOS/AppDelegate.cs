0c171c8f054038cddba0e8f2f1ad106f3bb9bf13
Mikhail Davydenko <mikhail.davidenko@nixsolutions.com>
2016-11-30 16:21:51 +0200

    Implement Sign In button on IOS

--- XFGoogleCalendarIntegration/XFGoogleCalendarIntegration/XFGoogleCalendarIntegration.iOS/AppDelegate.cs
+++ XFGoogleCalendarIntegration/XFGoogleCalendarIntegration/XFGoogleCalendarIntegration.iOS/AppDelegate.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using Google.Core;
+using Google.SignIn;
 using UIKit;
 
 namespace XFGoogleCalendarIntegration.iOS
@@ -22,10 +24,25 @@ namespace XFGoogleCalendarIntegration.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            string clientId = "192079722147-foetvqvjduvk6hh56uii4ep7lm5iiqu3.apps.googleusercontent.com";
+
+            //NSError configureError;
+            //Context.SharedInstance.Configure(out configureError);
+            //if (configureError != null)
+            //{
+                SignIn.SharedInstance.ClientID = clientId;
+                //SignIn.SharedInstance.ServerClientID = "192079722147-k89ga85nbbtj58p36msshrmh9i66phk9.apps.googleusercontent.com";
+            //}
+
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
+        }
+
+        public override bool OpenUrl(UIApplication application, NSUrl url, string sourceApplication, NSObject annotation)
+        {
+            return SignIn.SharedInstance.HandleUrl(url, sourceApplication, annotation);
         }
     }
 }
---
+++ XFGoogleCalendarIntegration/XFGoogleCalendarIntegration/XFGoogleCalendarIntegration.iOS/CustomRenderers/GoogleSignInButtonIos.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreGraphics;
+using Foundation;
+using Google.SignIn;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XFGoogleCalendarIntegration.CustomViews;
+using XFGoogleCalendarIntegration.iOS.CustomRenderers;
+
+[assembly: ExportRenderer(typeof(GoogleSignInButton), typeof(GoogleSignInButtonIos))]
+namespace XFGoogleCalendarIntegration.iOS.CustomRenderers
+{ 
+    public class GoogleSignInButtonIos : ViewRenderer, ISignInUIDelegate, ISignInDelegate
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<View> e)
+        {
+            var signInButton = new SignInButton();
+            signInButton.Frame = new CGRect(20, 100, 150, 44);
+            SignIn.SharedInstance.UIDelegate = this;
+            SignIn.SharedInstance.Delegate = this;
+            SignIn.SharedInstance.Scopes = new[]
+            {
+                "https://www.googleapis.com/auth/calendar"
+            };
+            this.SetNativeControl(signInButton);
+            base.OnElementChanged(e);
+        }
+
+        public void DidSignIn(SignIn signIn, GoogleUser user, NSError error)
+        {
+            if (error == null)
+            {
+                
+            }
+        }
+
+        [Export("signIn:presentViewController:")]
+        public void PresentViewController(SignIn signIn, UIViewController viewController)
+        {
+            UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(viewController, true, null);
+        }
+
+        [Export("signIn:dismissViewController:")]
+        public void DismissViewController(SignIn signIn, UIViewController viewController)
+        {
+            viewController.DismissViewController(true, null);
+        }
+    }
+}
