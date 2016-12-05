f17243af533923d14996ca336b437e46566d9d4d
Mikhail Davydenko <mikhail.davidenko@nixsolutions.com>
2016-12-05 15:22:41 +0200

    Add needed extensions

---
+++ XFGoogleCalendarIntegration/XFGoogleCalendarIntegration/XFGoogleCalendarIntegration/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFGoogleCalendarIntegration.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToRFC3339(this DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-ddTHH:mm:ssK");
+        }
+    }
+}
---
+++ XFGoogleCalendarIntegration/XFGoogleCalendarIntegration/XFGoogleCalendarIntegration/Extensions/ObjectExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace XFGoogleCalendarIntegration.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static string ToQueryString(this object obj)
+        {
+            var properties = obj.GetType().GetTypeInfo().DeclaredProperties
+                .Where(x => x.GetValue(obj, null) != null)
+                .Select(x =>
+                {
+                    string objToString;
+                    var value = x.GetValue(obj, null);
+                    if (value is DateTime)
+                    {
+                        objToString = ((DateTime) value).ToRFC3339();
+                    }
+                    else
+                    {
+                        objToString = value.ToString();
+                    }
+
+                    return ConvertFirstLetterToLowerCase(x.Name) + "=" + WebUtility.UrlEncode(objToString);
+                });
+
+            return "?" + string.Join("&", properties.ToArray());
+        }
+
+        private static string ConvertFirstLetterToLowerCase(string text)
+        {
+            return char.ToLowerInvariant(text[0]) + text.Substring(1);
+        }
+    }
+}
